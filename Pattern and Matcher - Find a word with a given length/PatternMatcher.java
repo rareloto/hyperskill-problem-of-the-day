e5cb49584d0b9c829971a6d5578711237994d969
Ramzel Renz Loto <lotoramzelrenz@gmail.com>
2020-07-12 13:53:38 +0800

    Create PatternMatcher.java

---
+++ Pattern and Matcher - Find a word with a given length/PatternMatcher.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PatternMatcher {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = Integer.parseInt(scanner.nextLine());
+        String line = scanner.nextLine();
+
+        boolean matchFound = Arrays.stream(line.split("[^A-Za-z]+")).anyMatch(word -> word.length() == size);
+        System.out.println(matchFound ? "YES" : "NO");
+    }
+}
